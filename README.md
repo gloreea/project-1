e54a82ae167f9472e037d21f3c2f33fc9ca82751
gloreea <aranda_gloree408@icloud.com>
2023-03-23 15:53:33 -0700

    whack a mole functionality

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 WHACK-A-MOLE
-Whack the moles whenever you see them, make sure not to hit the holes too early or late or it will close. If you run out of holes the game will end. The only way to win is to hit the moles 15 times in a row with no misses.
+Whack the moles whenever you see them! You have 30 seconds to score 15 points in order to win! If the timer runs out, game over!
 Use your cursor to click on each hole when you see a mole inside!
 
 ![Wireframe](wireframesketch.jpg)
@@ -14,7 +14,7 @@ MVP GOALS
 - When player loses, display game over
 - If player wins, display winner message
 - Allow mole to appear in circles 
-- Cover holes when a player clicks on or misses hole early/late
+
 
 STRETCH GOALS
 - Make cursor a hammer
--- scripts.js
+++ scripts.js
@@ -3,59 +3,92 @@ const mole = document.querySelectorAll(".mole")
 const holes = document.querySelectorAll(".hole")
 const score = document.querySelector("#score")
 const timer =  document.querySelector("#timer")
-
+const message = document.querySelector("#message")
+let resetMoleInterval 
+const reset = document.querySelector("#reset")
+let sound = new Audio('2019-12-09_-_Retro_Forest_-_David_Fesliyan.mp3')
 // timer
+let timerSecond = 30
 start.addEventListener("click", function () {
-    let timerSecond = 60
+    sound.play()
     timerInterval = setInterval (() => {
         timerSecond--
         timer.innerText = `:${timerSecond}`
         if (timerSecond <= 0) {
-        clearInterval(timerInterval)
+            clearInterval(timerInterval)
+            message.innerText = ("GAME OVER")
+            message.style.backgroundColor = "lightGreen"
+            clearTimeout(resetMoleInterval)
+            
+        } 
+        if (point == 15) {
+            clearInterval(timerInterval)
+            message.innerText = ("YOU WIN!")
+            message.style.backgroundColor = "lightGreen" 
+            clearTimeout(resetMoleInterval)
         }
     }, 1000)
+   // resetMoleInterval = setTimeout(resetMole, Math.random() *2000+2000)  
+    resetMoleInterval = setInterval(resetMole, 2000)
+    randomHole()
+   // clearInterval(start)
 })
 
-
-
+// function for holes (random)
+// change random hole to give correct mole class
 function randomHole() {
     const randomHole = holes[Math.floor(Math.random() * 9 )]
-    console.log(randomHole.firstElementChild)
-    randomHole.firstElementChild.classList.add("isVisible")
-    console.log(randomHole.firstElementChild)
-    
-    // holes.forEach(function (hole) {
-        // holes.removeChild("mole")
-        // })
-        
-    } 
-    function resetMole() {
+    randomHole.firstElementChild.style.visibility = "visible"
+    randomHole.firstElementChild.classList.add("correctMole")
+   // console.log(randomHole.firstElementChild)   
+    //correctMole = randomHole 
+}
+
+// change reset mole to remove correct mole 
+function resetMole() {
+    mole.forEach((element) => {
+        element.style.visibility = "hidden"
+        element.classList.remove("correctMole")
+    })
+    randomHole()
+   // resetMoleInterval = setTimeout(resetMole, Math.random() *2000+2000)   
+}
+
+let point = 0
+// create class for correct mole 
+// if you click correct mole, new mole spawns, timer resets
+// if you don't click correct mole, same result
+mole.forEach((element) => {
+    element.addEventListener("click", function (e) {
+        if (e.target.classList.contains("correctMole") ) {
+            // console.log("correctMole")
+            resetMole()
+            //setTimeout(resetMole, 1500)
+        }
+    })
+})
+
+// function for score
+           // holes.forEach((hole) => {
         mole.forEach((element) => {
-            element.classList.remove("isVisible")
+            element.addEventListener("mousedown", function (e) {
+                if(e.target.classList.contains("correctMole")) {
+                    point++
+                    score.innerText = `Score: ${point}` 
+                } 
+            })
         })
-        randomHole()
-        
-    }
-    
-    
-    start.addEventListener("click", function (e) {
-        // function for holes (random)
-        timerInterval;
-        setTimeout(resetMole, 2500)
-
-    // }
-
-
-
-
-
-
-    // function for score
-//     holes.addEventListener("onclick", function (e) {
-//         let point = 0
-//         if(holes === "onclick") {
-//             point++
-//             score.innerText = `${point}`
-//         }
-//     })
- })
+reset.addEventListener("click", () => {
+    clearInterval(timerInterval)
+    timerSecond = 30
+    timer.innerText = `:${timerSecond}`
+    point = 0
+    score.innerText = `Score: ${point}`
+    clearInterval(resetMoleInterval)
+    message.style.backgroundColor = ""
+    message.innerText = " "
+        mole.forEach((element) => {
+            element.style.visibility = "hidden"
+            element.classList.remove("correctMole")
+        })
+})
